d90d03b1b0db4666f7021c8f793e656352d9960a
daniels <livmanisdaniels@gmail.com>
2019-10-14 14:29:36 +0300

    test

---
+++ chapter01/asteroids1/rocket10.java
@@ -0,0 +1,29 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * Write a description of class rocket10 here.
+ */
+public class rocket10
+{
+    /* Instance variables - replace the example below with your own*/
+    private int x;
+
+    /**
+     * Constructor for objects of class rocket10
+     */
+    public rocket10()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     */
+    public int sampleMethod(int y)
+    {
+        /* Put your code here*/
+        return x + y;
+    }
+}
---
+++ chapter02-04/little-crab/Crab.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+
+/**
+ * This class defines a crab. Crabs live on the beach.
+ */
+public class Crab extends Animal
+{
+    public void act()
+    {
+        turn(5) ;
+        
+    }
+}
+        
+
+
